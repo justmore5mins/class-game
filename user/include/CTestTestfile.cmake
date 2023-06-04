1d304f7a0d2a389af9fdfd2aee5fe22cb7815991
justmore5mins <standby-nexuses0m@icloud.com>
2023-06-04 21:51:51 +0800

    add

---
+++ user/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/AustinYu/Library/CloudStorage/GoogleDrive-tno062789@gmail.com/我的雲端硬碟/HTML/遊戲的網頁/user/src/jsoncpp/include
+# Build directory: /Users/AustinYu/Library/CloudStorage/GoogleDrive-tno062789@gmail.com/我的雲端硬碟/HTML/遊戲的網頁/user/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
