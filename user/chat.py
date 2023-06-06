1a1f6a93c5c732ec838b10130c16fd2b05449cf5
justmore5mins <standby-nexuses0m@icloud.com>
2023-06-06 21:26:09 +0800

    add

--- user/chat.py
+++ user/chat.py
@@ -3,14 +3,21 @@ from json import load, dump
 
 app = Flask(__name__)
 
-@app.route('/chat.py',methods=['POST'])
+
+def updatechat(data):
+    with open("chat.json", "r", encoding="utf-8") as read:
+        dataread = load(read)
+        print(dataread)
+        dataread.update(data)
+    with open("chat.json", "w", encoding="utf-8") as write:
+        dump(dataread, write, indent=4)
+
+
+@app.route('/chat.py', methods=['POST'])
 def handle_post():
     data = request.get_json()
-    with open("chat.json","r",encoding="utf-8") as read:
-        dataread = load(read)
-        dataread.update(data)
-    with open("chat.json","w",encoding="utf-8") as write:
-        dump(dataread,write, indent=4)
-        
-while True:
-    handle_post()
+    updatechat(data)
+
+
+if __name__ == "__main__":
+    app.run()
