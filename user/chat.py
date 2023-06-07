45f53fcc7f54a7551c7527a5ab976a9bbc160844
justmore5mins <standby-nexuses0m@icloud.com>
2023-06-08 06:33:32 +0800

    add

--- user/chat.py
+++ user/chat.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-from json import load, dump
+from json import load, dump,loads
 
 app = Flask(__name__)
 CORS(app)
@@ -8,10 +8,15 @@ CORS(app)
 def updatechat(data):
     with open("chat.json", "r", encoding="utf-8") as read:
         dataread = load(read)
+        lastkey = list(load(read).keys())[-1]
+        lastkey = int(lastkey)
+        lastkey += 1
+        lastkey = str(lastkey)
+        writedata = {lastkey:dataread}
         dataread.update(data)
         print(dataread)
     with open("chat.json", "w", encoding="utf-8") as write:
-        dump(dataread, write, indent=4)
+        dump(writedata, write, indent=4)
 
 @app.route('/handle_post', methods=['POST'])
 def handle_post():
