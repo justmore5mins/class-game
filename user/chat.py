bff339b8bbaba04df15d0784f461466f30686cec
justmore5mins <standby-nexuses0m@icloud.com>
2023-06-08 06:45:06 +0800

    add

--- user/chat.py
+++ user/chat.py
@@ -8,15 +8,19 @@ CORS(app)
 def updatechat(data):
     with open("chat.json", "r", encoding="utf-8") as read:
         dataread = load(read)
-        lastkey = list(load(read).keys())[-1]
+        print(dataread)
+        """
+        lastkey = list()[-1]
         lastkey = int(lastkey)
         lastkey += 1
         lastkey = str(lastkey)
-        writedata = {lastkey:dataread}
         dataread.update(data)
+        writedata = {lastkey:dataread}
         print(dataread)
+        """
+        dataread.update(data)
     with open("chat.json", "w", encoding="utf-8") as write:
-        dump(writedata, write, indent=4)
+        dump(dataread, write, indent=4)
 
 @app.route('/handle_post', methods=['POST'])
 def handle_post():
